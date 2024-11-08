1b65b6b5d4ba962dc4a3d7713b8bb41a2a1d9145
Felix Kunze <f.kunze@campus.tu-berlin.de>
2024-11-08 10:41:29 +0100

    initial commit
    Release-As: 0.0.0

---
+++ host/config.py
@@ -0,0 +1,22 @@
+import os
+from pydantic import HttpUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+current_file_directory = os.path.dirname(os.path.abspath(__file__))
+env_file = os.path.join(
+    current_file_directory,
+    ".env.example",
+)
+
+
+class Config(BaseSettings):
+    read_interval_s: float = 1.0
+    serial_port: str
+    serial_baud_rate: int = 192000
+    backend_sensor_url: HttpUrl
+
+    
+    model_config = SettingsConfigDict(env_file=env_file)
+    
+settings = Config()
