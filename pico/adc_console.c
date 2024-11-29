9fa9b80f6cfed349fbfafef1e4de1b7f2b30c6fe
Felix Kunze <felix.kunze@t-online.de>
2024-11-29 13:37:06 +0100

    feat(adc_console): add offset correction and support for 2 sensors
    - Implemented offset correction by reading ground reference voltage
    - Added support for reading from 2 sensors (adc channels 0 and 1)
    - Removed unused sample buffer and adc_capture function
    - Set SMPS pin high for PWM mode to increase Vref stability
    
    BREAKING CHANGE: Listening to `0` and `1` on Serial to request sensor reading

--- pico/adc_console.c
+++ pico/adc_console.c
@@ -3,8 +3,7 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 
-#define N_SAMPLES 1000
-uint16_t sample_buf[N_SAMPLES];
+const float conversion_factor = 3.3f / (1 << 12);
 
 void printhelp()
 {
@@ -12,14 +11,18 @@ void printhelp()
     puts("s\t: Sample once");
 }
 
-void __not_in_flash_func(adc_capture)(uint16_t *buf, size_t count)
-{
-    adc_fifo_setup(true, false, 0, false, false);
-    adc_run(true);
-    for (size_t i = 0; i < count; i = i + 1)
-        buf[i] = adc_fifo_get_blocking();
-    adc_run(false);
-    adc_fifo_drain();
+void read_with_offset_correction(int adc_channel) {
+    // Read Signal
+    adc_select_input(adc_channel);
+    const uint32_t signal = adc_read();
+    // Read Ground reference to correct offset
+    adc_select_input(2);
+    const uint32_t ground = adc_read();
+    // correct the signal
+    const uint32_t corrected_signal = signal - ground;
+    const float voltage = corrected_signal * conversion_factor;
+    // Calculate and print Voltage to serial
+    printf("%f\n", voltage);
 }
 
 int main(void)
@@ -27,7 +30,6 @@ int main(void)
     stdio_init_all();
     adc_init();
     adc_set_temp_sensor_enabled(false);
-    adc_select_input(0);
 
     // Set all pins to input (as far as SIO is concerned)
     gpio_set_dir_all_bits(0);
@@ -43,10 +45,14 @@ int main(void)
 
 // initialize onboard LED default on
 #ifdef PICO_DEFAULT_LED_PIN
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     gpio_put(PICO_DEFAULT_LED_PIN, 1);
 #endif
+
+    // Set SMPS Pin high for PWM_mode to increase Vref stability
+    gpio_init(23);
+    gpio_set_dir(23, GPIO_OUT);
+    gpio_put(23, 1);
+
 
     printf("\n===========================\n");
     printf("RP2040 ADC and Test Console\n");
@@ -58,13 +64,14 @@ int main(void)
         char recievedChar = getchar();
         switch (recievedChar)
         {
-        case 's':
+        case '0':
         {
-            uint32_t result = adc_read();
-            const float conversion_factor = 3.3f / (1 << 12);
-            printf("%f\n", result, result * conversion_factor);
+            read_with_offset_correction(0);
             break;
         }
+        case '1':
+            read_with_offset_correction(1);
+            break;
         case '\n':
         case '\r':
             break;
